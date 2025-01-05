0292c2f9af6b63587fa169bbc4747167d34cc0eb
omerc <unxgx@student.kit.edu>
2025-01-05 21:28:04 +0100

    new design, new folders for a better structure, using bootstrap, background color and text color according to prototype, new font, navbar

--- server/myLoad/views.py
+++ server/myLoad/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def start(request):
-    return render(request, 'start.html')
+    return render(request, 'pages/start.html',  {'show_buttons': True})
 
 def settings(request):
-    return render(request, 'settings.html')
+    return render(request, 'pages/settings.html')
 
 
 
