665b51ac988d539dce956d4924b49fe5a3a8626c
uknpi <uknpi@student.kit.edu>
2025-01-26 01:39:24 +0100

    Just changed the design of the startingButton and the baseline text

--- app/view/baselinePage.py
+++ app/view/baselinePage.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
 import sys
 
 class BaselineWidget(QWidget):
@@ -13,9 +15,17 @@ class BaselineWidget(QWidget):
         self.label = QLabel()
         self.label.setText("Baseline is being measured")
 
+        # Text zentrieren
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Text größer machen
+        font = QFont("Arial", 15)  # Schriftart und Schriftgröße
+        self.label.setFont(font)
+
+        # Optional: Weitere Text-Styles mit StyleSheet
+        self.label.setStyleSheet("color: #333;")  # Textfarbe ändern
+
         layout.addWidget(self.label)
 
         self.setLayout(layout)
         self.setWindowTitle('Baseline Widget')
-
-        
