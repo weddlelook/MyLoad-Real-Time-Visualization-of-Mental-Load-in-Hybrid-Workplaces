a096fa824673d5cfa4d5f8dcc770ee5fc9a6e73a
Clara <udvbk@student.kit.edu>
2025-01-26 14:51:10 +0100

    cleaning up repository, removing server folder

--- server/myLoad/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.start, name='myload-start'),
-    path('settings/', views.settings, name='myload-settings'),
-    path('meeting/', views.start_meeting, name='start-meeting'),
-]
