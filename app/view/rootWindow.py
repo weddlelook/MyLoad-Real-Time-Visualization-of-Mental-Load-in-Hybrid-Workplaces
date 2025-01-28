c6a5a2d9041a87e145ce6728672c945dc0522287
Clara <udvbk@student.kit.edu>
2025-01-28 21:24:14 +0100

    seperating rootWindow and mainWidget

--- app/view/rootWindow.py
+++ app/view/rootWindow.py
@@ -1,20 +1,13 @@
 from PyQt6.QtWidgets import QStackedLayout, QMainWindow, QVBoxLayout, QWidget, QToolBar, QLineEdit
-from PyQt6.QtGui import QFontDatabase, QFont
-from PyQt6.QtGui import QAction, QIcon
-from app.view.startWidget import StartWidget
-from app.view.plotWidget import EEGPlotWidget
-from app.view.baselinePage import BaselineWidget
-from app.view.maxtestPage import MaxtestPage
-from app.view.settingsPage import SettingsWidget
-from app.view.startBaselinePage import StartBaselinePage
-from app.view.retrospectivePage import RetrospectivePage
-from app.view.resultsPage import ResultsPage
+from PyQt6.QtGui import QFontDatabase, QFont, QAction, QIcon
+from app.view.mainWidget import MainWidget
 import os
 
 class RootWindow(QMainWindow):
     def __init__(self, settings):
         super().__init__()
-        self.setWindowTitle("")
+
+        self.setWindowTitle("MyLoad")
         self.settings = settings
         self.main_window = MainWidget()
         self.setCentralWidget(self.main_window)
@@ -46,80 +39,3 @@ class RootWindow(QMainWindow):
             self.findChild(QToolBar).setVisible(True)
         elif not show:
             self.findChild(QToolBar).setVisible(False)
-
-
-
-class MainWidget(QWidget):
-
-    """
-    This class represents the main Window of this application.
-    It holds two child-Widgets:
-    > A stack holding all Stages of the Lifecycle
-    > A Settings - Widget
-    that can be switched with the provided methods
-    """
-
-    def __init__(self):
-        super().__init__()
-
-        # Main layout container
-        self.main_layout = QVBoxLayout()
-        self.setLayout(self.main_layout)
-
-        # Setting up Settings
-        self.settings = SettingsWidget()
-
-        # Setting up Stack
-        self.stack = QWidget()
-        self.stack_layout = QStackedLayout()
-        self.stack.setLayout(self.stack_layout)
-
-        # Adding to main Widget
-        self.main_layout.addWidget(self.stack)
-        self.main_layout.addWidget(self.settings)
-
-        # Dict for all registered pages
-        self.pages = {}
-
-        # Registering all pages 
-        # NOTE: We could decide to not register all pages here, but instead register them 
-        #       as we go along in the application lifecycle, but for now, we will register them all here
-        self._register_page(StartWidget(), "start")
-        self._register_page(EEGPlotWidget(), "plot")
-        self._register_page(BaselineWidget(), "baseline")
-        self._register_page(StartBaselinePage(), "baselineStartPage")
-        self._register_page(MaxtestPage(), "maxtest")
-        self._register_page(RetrospectivePage("h5_session_files"), "retrospective")
-        self._register_page(ResultsPage(), "result")
-
-
-        self.settings.hide()
-
-    def _register_page(self, child: QWidget, page_name: str):
-        """Registers a page with the main window."""
-        self.stack_layout.addWidget(child)
-        self.pages[page_name] = child
-
-    def set_page(self, page_name: str) -> QWidget:
-        """
-        Sets the current index of the main window to the page with the given name.
-        Returns the Widget.
-        """
-        try:
-            widget = self.pages[page_name]
-            widget_index = self.stack_layout.indexOf(widget)
-            self.stack_layout.setCurrentIndex(widget_index)
-            return widget
-        except KeyError as e:
-            print(f"Error during page change: {e}")
-            return None
-
-    def toggle_settings(self) -> QWidget:
-        if self.settings.isVisible():
-            self.settings.hide()
-            self.stack.show()
-            return self.stack
-        else:
-            self.stack.hide()
-            self.settings.show()
-            return self.settings
